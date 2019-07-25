48d1d0c9c36f168c4fb498358f4a41f749ab2c71
MarkTeqDeveloper <46553169+MarkTeqDeveloper@users.noreply.github.com>
2019-07-25 18:14:24 +0530

    Update ShowToast.java

--- showtoast/src/main/java/com/markteq/showtoast/ShowToast.java
+++ showtoast/src/main/java/com/markteq/showtoast/ShowToast.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class ShowToast {
 
-    public static void showToast(Context context,String msg){
+    public static void Utils(Context context,String msg){
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 
